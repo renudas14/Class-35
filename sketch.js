ab3f4135d2561026abf6c2bc2eb7a2c62c7930a9
renudas14 <70806401+renudas14@users.noreply.github.com>
2020-12-05 18:56:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var ball;
+var database,position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    balllocation = database.ref("car/position")
+    balllocation.on("value",readpos)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readpos(data){
+    position = data.val()
+    ball.x=position.x;
+    ball.y = position.y;
+}
+
+function changePosition(x,y){
+    database.ref("car/position").set({
+     x:position.x+x,
+     y:position.y+y
+    })
+}
